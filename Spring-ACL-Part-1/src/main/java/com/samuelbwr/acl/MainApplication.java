8dd7576cb48707300f9fddd6ef5b47ffacf7fe90
Samuel Berwanger <samuelbwr@gmail.com>
2017-09-16 18:37:05 -0300

    First commit

---
+++ Spring-ACL-Part-1/src/main/java/com/samuelbwr/acl/MainApplication.java
@@ -0,0 +1,12 @@
+package com.samuelbwr.acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+}
